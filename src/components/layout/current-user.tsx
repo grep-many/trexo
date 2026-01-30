5d75eb2eb60e12befea544790f5d739a0ce57284
Manish <manishkumar9540898@gmail.com>
2026-01-30 07:25:22 +0530

    feat: add current-user component

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,20 +1,64 @@
-import { Popover } from "antd";
+import { Button, Popover } from "antd";
+import { SettingOutlined } from "@ant-design/icons";
 import CustomAvatar from "../custom-avatar";
-// import { User } from "@/graphql/schema.types";
-// import { useGetIdentity } from "@refinedev/core";
+import { User } from "@/graphql/schema.types";
+import { useGetIdentity } from "@refinedev/core";
+import { Text } from "../text";
+import React from "react";
 
 const CurrentUser = () => {
-  // const { data: user } = useGetIdentity<User>();
+  const [isOpen, setIsOpen] = React.useState(false);
+  const { data: user } = useGetIdentity<User>();
+
+  const content = (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <Text strong style={{ padding: "12px 20px" }}>
+        {user?.name || "Guest"}
+      </Text>
+      <div
+        style={{
+          borderTop: "1px solid #d9d9d9",
+          padding: "4px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "4px",
+        }}
+      >
+        <Button
+          style={{ textAlign: "left" }}
+          icon={<SettingOutlined />}
+          type="text"
+          block
+          onClick={() => setIsOpen(true)}
+        >
+          Account Settings
+        </Button>
+      </div>
+    </div>
+  );
+
   return (
     <>
       <Popover
         placement="bottomRight"
         trigger="click"
-        // overlayInnerStyle={{ padding: 0 }}
-        // overlayStyle={{ zIndex: 999 }}
+        styles={{
+          body: { padding: 0 },
+          root: { zIndex: 999 },
+        }}
+        content={content}
       >
-        <CustomAvatar name="JM" />
+        <CustomAvatar
+          name={user?.name || "Guest"}
+          src={user?.avatarUrl}
+          size={"default"}
+          style={{ cursor: "pointer" }}
+        />
       </Popover>
+      {/* 
+        TODO: Create a accountSettings
+        {user && <AccountSettings opened={isOpen} setIsOpened={setIsOpen} user={user}/>} 
+      */}
     </>
   );
 };
