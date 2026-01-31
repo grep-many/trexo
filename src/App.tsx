97d1d8d9fca879d31c3ce88c3fdce738be25a4bb
Manish <manishkumar9540898@gmail.com>
2026-01-31 08:06:46 +0530

    feat: finish header

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import { ForgotPassword, Home, Login, Register } from "./pages";
 import Layout from "./components/layout";
+import { resources } from "./config/resources";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
               notificationProvider={useNotificationProvider}
               routerProvider={routerProvider}
               authProvider={authProvider}
+              resources={resources}
               options={{
                 syncWithLocation: true,
                 warnWhenUnsavedChanges: true,
